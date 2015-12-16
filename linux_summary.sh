c56fc1193a210083931a529a3e55501f274a8bf3
Jeremy Schaub <schaubjeremy@gmail.com>
2015-12-16 13:15:08 -0600

    Added lspci

--- linux_summary.sh
+++ linux_summary.sh
@@ -7,9 +7,9 @@ echo Creating $HTML_FN
 rm -f $HTML_FN
 
 ALL_COMMANDS=("hostname" "lscpu" "cat /proc/cpuinfo" "cat /proc/meminfo" "uname -a" \
-    "lsblk" "df -h" "cat /etc/issue" "hostname -I")
+    "lsblk" "lspci" "df -h" "cat /etc/issue" "hostname -I")
 ALL_TAGS=("TAG_HOSTNAME" "TAG_LSCPU" "TAG_CPUINFO" "TAG_MEMINFO" "TAG_LINUX" \
-    "TAG_LSBLK" "TAG_DF" "TAG_OS" "TAG_IPADDR")
+    "TAG_LSBLK" "TAG_LSPCI" "TAG_DF" "TAG_OS" "TAG_IPADDR")
 
 INDEX=$(cat template.html)
 
@@ -18,7 +18,7 @@ do
     COMMAND=${ALL_COMMANDS[i]}
     TAG=${ALL_TAGS[i]}
     # echo command is \"$COMMAND\", search is \"${TAG}\"
-    $COMMAND > tmp
+    $COMMAND 2>&1 > tmp
     DATA=$(cat tmp | perl -pe "s/\n/\<br\/\>/g")  # newlines --> line breaks
     DATA=$(echo $DATA | perl -pe "s/<br\/>$//")   # Remove trailing line break
     rm tmp
