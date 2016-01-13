b5caf7d4a8d3e30af1e224f7cefbeb85910f7746
Jeremy D. Schaub <schaubj@tulgb008.cluster.com>
2016-01-13 14:04:44 -0500

    Added datetime and gpu

--- linux_summary.sh
+++ linux_summary.sh
@@ -7,9 +7,9 @@ echo Creating $HTML_FN
 rm -f $HTML_FN
 
 ALL_COMMANDS=("hostname" "lscpu" "cat /proc/cpuinfo" "cat /proc/meminfo" "uname -a" \
-    "lsblk" "lspci" "df -h" "cat /etc/issue" "hostname -I")
+    "lsblk" "lspci" "df -h" "cat /etc/issue" "hostname -I" "nvidia-smi" "date +%Y%m%d-%H%M%S")
 ALL_TAGS=("TAG_HOSTNAME" "TAG_LSCPU" "TAG_CPUINFO" "TAG_MEMINFO" "TAG_LINUX" \
-    "TAG_LSBLK" "TAG_LSPCI" "TAG_DF" "TAG_OS" "TAG_IPADDR")
+    "TAG_LSBLK" "TAG_LSPCI" "TAG_DF" "TAG_OS" "TAG_IPADDR" "TAG_GPU", "TAG_DATETIME")
 
 INDEX=$(cat template.html)
 
