21b3b4e6bd5343d931d5cb8179a15acc3f5d0c25
Jeremy Schaub <jschaub30@users.noreply.github.com>
2017-12-20 14:12:59 -0600

    Update linux_summary.sh

--- linux_summary.sh
+++ linux_summary.sh
@@ -8,7 +8,7 @@ rm -f $HTML_FN
 
 ALL_COMMANDS=("hostname" "ifconfig -a" "lscpu" "cat /proc/cpuinfo" "cat /proc/meminfo" "uname -a" \
     "lsblk" "lspci" "df -h" "cat /etc/issue" "hostname -I" "nvidia-smi" \
-    "date +%Y%m%d-%H%M%S" "lstopo")
+    "date +%Y%m%d-%H%M%S" "lstopo-no-graphics")
 ALL_TAGS=("TAG_HOSTNAME" "TAG_IFCONFIG" "TAG_LSCPU" "TAG_CPUINFO" "TAG_MEMINFO" "TAG_LINUX" \
     "TAG_LSBLK" "TAG_LSPCI" "TAG_DF" "TAG_OS" "TAG_IPADDR" "TAG_GPU" \
     "TAG_DATETIME" "TAG_LSTOPO")
